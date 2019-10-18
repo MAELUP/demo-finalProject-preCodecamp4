8f54c3bd177845103bc4de2dd215682aa01513b9
MAELUP <pluam2541@gmail.com>
2019-10-18 22:00:20 +0700

    debug id name

--- jsSheet.js
+++ jsSheet.js
@@ -7,36 +7,36 @@ function outText(x) {
 }
 
 function clickText(x){
-let myid = x ;
-switch(myid){
+let myid;
+switch(x){
     case myFullName: 
-        alert()
+        myid = "myFullName";
         break;
     case myName:
-        alert()
+        myid = "myName";
         break;
-    case myEdu-1:
-        alert()
+    case myEdu_1:
+        myid = "mymyEdu_1";
         break;
-    case myEduPro-1:
-        alert()
+    case myEduPro_1:
+        myid = "mmyEduPro_1";
         break;
-    case myEduDate-1:
-        alert()
+    case myEduDate_1:
+        myid = "myEduDate_1";
         break;
-    case myEdu-2:
-        alert()
+    case myEdu_2:
+        myid = "mmyEdu_2";
         break;
-    case myEduPro-2:
-        alert()
+    case myEduPro_2:
+        myid = "myEduPro_2";
         break;
-    case myEduDate-2:
-        alert()
+    case myEduDate_2:
+        myid = "myEduDate_2";
         break;
     default:
         alert("not found")
         break;
 }
+alert(myid)
 
-
-}
+} 
