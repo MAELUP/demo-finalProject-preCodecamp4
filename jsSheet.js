c32c863681c26bd1a7a34cbdbd24946962bcc674
MAELUP <pluam2541@gmail.com>
2019-10-19 01:30:47 +0700

    add change function

--- jsSheet.js
+++ jsSheet.js
@@ -3,40 +3,52 @@ function overText(x) {
 }
 
 function outText(x) {
-    x.style.color = "#FFFFFF";
+    x.style.color = "#f0f8ff";
 }
 
 function clickText(x){
-let myid;
-switch(x){
-    case myFullName: 
-        myid = "myFullName";
-        break;
-    case myName:
-        myid = "myName";
-        break;
-    case myEdu_1:
-        myid = "mymyEdu_1";
-        break;
-    case myEduPro_1:
-        myid = "mmyEduPro_1";
-        break;
-    case myEduDate_1:
-        myid = "myEduDate_1";
-        break;
-    case myEdu_2:
-        myid = "mmyEdu_2";
-        break;
-    case myEduPro_2:
-        myid = "myEduPro_2";
-        break;
-    case myEduDate_2:
-        myid = "myEduDate_2";
-        break;
-    default:
-        alert("not found")
-        break;
-}
-alert(myid)
-
+    let myid;
+    let myEdit;
+    let newText;
+    switch(x){
+        case myFullName: 
+            myid = "myFullName";
+            myEdit = "Full Name";
+            break;
+        case myName:
+            myid = "myName";
+            myEdit = "Name";
+            break;
+        case myEdu_1:
+            myid = "mymyEdu_1";
+            myEdit = "Acedemy";
+            break;
+        case myEduPro_1:
+            myid = "mmyEduPro_1";
+            myEdit = "Study Program";
+            break;
+        case myEduDate_1:
+            myid = "myEduDate_1";
+            myEdit = "Date of Study";
+            break;
+        case myEdu_2:
+            myid = "mmyEdu_2";
+            myEdit = "Acedemy";
+            break;
+        case myEduPro_2:
+            myid = "myEduPro_2";
+            myEdit = "Study Program";
+            break;
+        case myEduDate_2:
+            myid = "myEduDate_2";
+            myEdit = "Date of Study";
+            break;
+        default:
+            alert("not found")
+            break;
+    }
+    newText = prompt('Edit your '+myEdit);
+    if(newText != null){
+        document.getElementById(myid).innerHTML = newText;
+    }
 } 
