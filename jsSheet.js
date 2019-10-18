6f23b5f471081757e1054ca024ccb82808d86444
MAELUP <pluam2541@gmail.com>
2019-10-18 21:50:38 +0700

    add switch

--- jsSheet.js
+++ jsSheet.js
@@ -6,8 +6,37 @@ function outText(x) {
     x.style.color = "#FFFFFF";
 }
 
-function clickText(){
-
+function clickText(x){
+let myid = x ;
+switch(myid){
+    case myFullName: 
+        alert()
+        break;
+    case myName:
+        alert()
+        break;
+    case myEdu-1:
+        alert()
+        break;
+    case myEduPro-1:
+        alert()
+        break;
+    case myEduDate-1:
+        alert()
+        break;
+    case myEdu-2:
+        alert()
+        break;
+    case myEduPro-2:
+        alert()
+        break;
+    case myEduDate-2:
+        alert()
+        break;
+    default:
+        alert("not found")
+        break;
+}
 
 
 }
